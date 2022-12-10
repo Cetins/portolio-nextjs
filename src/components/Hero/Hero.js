3caf60082cb572d35af779a3e746b93c2044370c
Cetins <69481095+Cetins@users.noreply.github.com>
2022-12-10 09:45:33 +0000

    hero basics added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dignissim iaculis imperdiet. Quisque feugiat massa nulla, in eleifend neque tincidunt nec. Fusce aliquet nulla non velit gravida, eu dapibus odio convallis. Pellentesque a eros ut erat maximus tempus. Quisque sit amet porta ante, at consequat ligula. In hac habitasse platea dictumst.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
