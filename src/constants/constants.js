23ca6973fd62cbf55525ae4413fb8fe3a0de3424
Cetins <69481095+Cetins@users.noreply.github.com>
2022-12-11 11:47:27 +0000

    tags replaced with imgs, dummy data replaced

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,36 +1,49 @@
+const css = '/images/tech-logos/css.png';
+const flask = '/images/tech-logos/flask.png';
+const html5 = '/images/tech-logos/html5.png';
+const java = '/images/tech-logos/java.png';
+const js = '/images/tech-logos/js.png';
+const junit5 = '/images/tech-logos/junit5.png';
+const mongodb = '/images/tech-logos/mongodb.png';
+const postgresql = '/images/tech-logos/postgresql.png';
+const python = '/images/tech-logos/python.png';
+const react = '/images/tech-logos/react.png';
+const spring = '/images/tech-logos/spring.png';
+const sql = '/images/tech-logos/sql-server.png';
+
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+    title: 'Sales Tracker',
+    description: "Sales tracking app for small businesses who offer services and products such as barbershops, hairdressers. This app stores sales data, tracks stock for products and calculates commissions for employees. I'm still coding this project from time to time so please check the README.md on the link for updated information.",
       image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
+      tags: [ react, js, mongodb, html5, css ],
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 0,
   },
   {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+    title: 'Code BNB',
+    description:"This is the capstone project for my CodeClan education. Users can host their properties and guests can book and review their experiences.  You can check the README.md on the links for more detailed information.",
     image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
+    tags: [ js, react, java, spring, sql, html5, css ],
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 1,
   },
   {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
-      tags: ['React', 'WebRTC'],
+    title: 'Gymmy',
+    description: "Local Gym member and course management system. This app can add/track/edit members and courses. Users can book members to a course. You can check the README.md on the link for more detailed information.",
+      image: '/images/3.png',
+      tags: [ python, flask, postgresql, html5, css ],
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 2,
   },
   {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
+    title: 'Syncfusion Dashboard',
+    description: "Front-end design for commercial dashboard with financial graphs. ",
+    image: '/images/4.png',
+    tags: [ js, react, mongodb, html5, css ],
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 3,
