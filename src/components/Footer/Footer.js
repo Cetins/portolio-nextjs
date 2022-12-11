44106a53bdcddb070bb8060a7a0403d388146132
Cetins <69481095+Cetins@users.noreply.github.com>
2022-12-11 11:57:35 +0000

    insta social link replaced with medium blog link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillMediumCircle, AiFillLinkedin } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
@@ -19,17 +19,17 @@ const Footer = () => {
       </LinkList>
       <SocialIconsContainer>
         <CompanyContainer>
-          <Slogan>Runs with Coffee, Silence, </Slogan>
+          <Slogan>Runs with coffee, driven by puzzles, requires challange</Slogan>
         </CompanyContainer>
         <SocialContainer>
-          <SocialIcons href='https://github.com'>
+          <SocialIcons href='https://github.com/Cetins'>
             <AiFillGithub size="3rem"/>
           </SocialIcons>
-          <SocialIcons href='https://linkedin.com'>
+          <SocialIcons href='https://linkedin.com/in/cetins'>
             <AiFillLinkedin size="3rem"/>
           </SocialIcons>
-          <SocialIcons href='https://instagram.com'>
-            <AiFillInstagram size="3rem"/>
+          <SocialIcons href='https://medium.com/@cetins'>
+            <AiFillMediumCircle size="3rem"/>
           </SocialIcons>
         </SocialContainer>
       </SocialIconsContainer>
