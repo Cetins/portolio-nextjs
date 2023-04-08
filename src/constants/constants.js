13c8e1d8bb8ffef1f220688926b26e026888d305
Cetins <69481095+Cetins@users.noreply.github.com>
2023-04-08 10:58:56 +0100

    editorial content changed, removed image constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,53 +1,42 @@
-const css = '/images/tech-logos/css.png';
-const flask = '/images/tech-logos/flask.png';
-const html5 = '/images/tech-logos/html5.png';
-const java = '/images/tech-logos/java.png';
-const js = '/images/tech-logos/js.png';
-const junit5 = '/images/tech-logos/junit5.png';
-const mongodb = '/images/tech-logos/mongodb.png';
-const postgresql = '/images/tech-logos/postgresql.png';
-const python = '/images/tech-logos/python.png';
-const react = '/images/tech-logos/react.png';
-const spring = '/images/tech-logos/spring.png';
-const sql = '/images/tech-logos/sql-server.png';
+
 
 export const projects = [
   {
     title: 'Sales Tracker',
     description: "Sales tracking app for small businesses who offer services and products such as barbershops, hairdressers. This app stores sales data, tracks stock for products and calculates commissions for employees. I'm still coding this project from time to time so please check the README.md on the link for updated information.",
       image: '/images/1.png',
-      tags: [ react, js, mongodb, html5, css ],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+      tags: [ 'js', 'react', 'express', 'mongodb' ],
+    source: 'https://github.com/Cetins/sale-tracker-client',
+    visit: '',
     id: 0,
+  },
+  {
+    title: 'Demon Clan',
+    description: "Demons vs Players. A demonstration of OOP with Java. One of the most entertaining projects both in concept and technical level. This project is a good example of Java inheritance with Abstract classes, sub-classes and Interfaces. You can check the README.md on the link for more detailed information",
+    image: '/images/4.png',
+    tags: [ 'java', 'spring', 'junit', 'sql' ],
+    source: 'https://github.com/Cetins/DemonClan',
+    visit: '',
+    id: 3,
   },
   {
     title: 'Code BNB',
     description:"This is the capstone project for my CodeClan education. Users can host their properties and guests can book and review their experiences.  You can check the README.md on the links for more detailed information.",
     image: '/images/2.png',
-    tags: [ js, react, java, spring, sql, html5, css ],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    tags: [ 'js', 'react', 'java', 'spring', 'sql' ],
+    source: 'https://github.com/Cetins/bnb-front-end',
+    visit: '',
     id: 1,
   },
   {
     title: 'Gymmy',
     description: "Local Gym member and course management system. This app can add/track/edit members and courses. Users can book members to a course. You can check the README.md on the link for more detailed information.",
       image: '/images/3.png',
-      tags: [ python, flask, postgresql, html5, css ],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+      tags: [ 'python', 'flask', 'sql' ],
+    source: 'https://github.com/Cetins/gymmyc',
+    visit: '',
     id: 2,
-  },
-  {
-    title: 'Syncfusion Dashboard',
-    description: "Front-end design for commercial dashboard with financial graphs. ",
-    image: '/images/4.png',
-    tags: [ js, react, mongodb, html5, css ],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 3,
-  },
+  }
 ];
 
 export const TimeLineData = [
